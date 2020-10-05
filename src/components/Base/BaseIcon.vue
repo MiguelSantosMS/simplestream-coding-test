965e647375e61970ee9185ec1a9933ad3c0a8f66
Miguel Santos <miguel.santos@tripleplay.tv>
2020-10-05 08:46:54 +0100

    Adding the icon assets and base icon component

---
+++ src/components/Base/BaseIcon.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="base-icon-container" @click="clickAction">
+        <img :src="resolveIconPath" :alt="iconName"/>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'BaseIcon',
+        props: {
+            iconName: String,
+            iconExtension: {
+                type: String,
+                default: 'svg'
+            },
+            clickAction: {
+                type: Function
+            }
+        },
+        computed: {
+            resolveIconPath () {
+                return require(`../../assets/icons/${this.iconName}.${this.iconExtension}`)
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
