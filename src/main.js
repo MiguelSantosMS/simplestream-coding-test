5dc34a352f16f20a4d2d6d001ac77515fb62b2d4
Miguel Santos <miguel.santos@tripleplay.tv>
2020-10-05 08:43:35 +0100

    Separating routes into a single file

--- src/main.js
+++ src/main.js
@@ -2,6 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
+// Import the global styling
+import './styles/basics/base.scss'
+
 Vue.config.productionTip = false
 
 new Vue({
