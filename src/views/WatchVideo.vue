08bed6c78a82fce016fd2ec8d4ab785f3c9a1b1e
Miguel Santos <miguel.santos@tripleplay.tv>
2020-10-05 09:02:47 +0100

    Implementing WatchVideo

--- src/views/WatchVideo.vue
+++ src/views/WatchVideo.vue
@@ -1,5 +1,33 @@
 <template>
-    <div class="about">
-        <h1>This is an about page</h1>
+    <div class="watch-video">
+        <h1 class="watch-video__title view-title">Watch Video</h1>
+        <h2 class="watch-video__video-title">{{ videoToWatch.title }}</h2>
+        <div class="watch-video__thumbnail-container">
+            <base-video-thumbnail class="watch-video-thumbnail" :video="videoToWatch"></base-video-thumbnail>
+            <base-icon class="watch-video__play-icon"
+                       icon-name="btn-play"
+                       icon-extension="png"
+                       :click-action="playVideo"></base-icon>
+        </div>
     </div>
 </template>
+
+<script>
+    import BaseVideoThumbnail from '@/components/Base/BaseVideoThumbnail'
+    import BaseIcon from '@/components/Base/BaseIcon'
+
+    export default {
+        name: 'WatchVideo',
+        components: { BaseIcon, BaseVideoThumbnail },
+        props: {
+            videoToWatch: {
+                type: Object
+            }
+        },
+        methods: {
+            playVideo () {
+                return true
+            }
+        }
+    }
+</script>
