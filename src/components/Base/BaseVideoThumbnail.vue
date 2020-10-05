4461ec2c37a8b74235e60e92110867a7b7e409f3
Miguel Santos <miguel.santos@tripleplay.tv>
2020-10-05 08:57:20 +0100

    Implementing BaseVideoThumbnail

---
+++ src/components/Base/BaseVideoThumbnail.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="base-video-thumbnail-container thumbnail">
+        <img class="base-video-thumbnail__image" :src="video.poster" :alt="video.title"/>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'BaseVideoThumbnail',
+        props: {
+            video: {
+                type: Object
+            }
+        }
+    }
+</script>
